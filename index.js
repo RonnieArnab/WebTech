e6676f990d0dd8ce5c2f57266475514a93c34a81
Arnab Ghosh <as.920037arnabghosh@gmail.com>
2023-03-26 00:47:36 +0530

    7th Commit

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ let searchFrom =  document.querySelector(".search-form") ;
 document.querySelector("#search-btn").onclick = () => {
     searchFrom.classList.toggle('active') ;
 }
+
+let shoppingCart =  document.querySelector(".shopping-cart") ;
+document.querySelector("#cart-shopping-btn").onclick = () => {
+    shoppingCart.classList.toggle('active') ;
+}
